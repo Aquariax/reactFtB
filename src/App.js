bb48ea296299ae0f97598476d68bc70072e49c76
aquariax <roberthrrs8@gmail.com>
2020-11-15 20:58:08 -0500

    created user items deconstructed state in Useritem for cleaner look

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import Navbar from "./components/layout/Navbar"
+import Useritem from "./components/users/Useritem"
 import './App.css';
+
 
 class App extends Component {
   render(){
+    // call or set variables outside of return
+    const name = 'robert'
     return (
-      <div className="App">
-        <h1>hello from react</h1>
+    <div className='App'>
+        <Navbar/>      
+        <Useritem/>
       </div>
+      
     );
   }
 
