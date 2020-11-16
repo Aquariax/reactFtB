96b710e3c96deaee0bad33f3b5ee8afab26eaf8f
aquariax <roberthrrs8@gmail.com>
2020-11-16 05:26:52 -0500

    created search component with user search functionality

---
+++ src/components/users/Search.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+export class Search extends Component {
+    state={
+        text:""
+    };
+    onChange =(typedIn)=>{
+        this.setState({[typedIn.target.name]: typedIn.target.value})
+    }
+    onSubmit=(submit)=>{
+        submit.preventDefault();
+        this.props.searchUsers(this.state.text);
+        this.setState({text:''})
+    }
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.onSubmit} className="form">
+                    <input type="text" name="text" placeholder="Search Users..."
+                    value={this.state.text}
+                    onChange={this.onChange}/>
+                    <input type="submit" value="Search" className="btn btn-dark btn-block"/>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Search
